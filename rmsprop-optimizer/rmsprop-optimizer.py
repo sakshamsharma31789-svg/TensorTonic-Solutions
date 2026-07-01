ee77817164ab8a035634a9c61b34f2a68966dd1a
sakshamsharma31789-svg <sakshamsharma31789@gmail.com>
2026-07-01 23:23:59 +0530

    Solve rmsprop-optimizer

---
+++ rmsprop-optimizer/rmsprop-optimizer.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def rmsprop_step(w, g, s, lr=0.001, beta=0.9, eps=1e-8):
+    w = np.array(w)
+    g = np.array(g)
+    s = np.array(s)
+    s = beta * s + (1-beta) * (g ** 2)
+    w = w - (lr/np.sqrt(s+eps)) * g
+    return w,s
+    pass
