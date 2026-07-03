540310822590cc7de2a0652eae5cf6a4b369074e
sakshamsharma31789-svg <sakshamsharma31789@gmail.com>
2026-07-03 16:38:07 +0530

    Solve cross-entropy-loss

--- cross-entropy-loss/cross-entropy-loss.py
+++ cross-entropy-loss/cross-entropy-loss.py
@@ -8,7 +8,6 @@ def cross_entropy_loss(y_true, y_pred):
     y_pred = np.asarray(y_pred)
     N = len(y_true)
     correct_class_probs = y_pred[np.arange(N),y_true]
-    # Write code here
     loss = -np.mean(np.log(correct_class_probs))
     return loss
     pass
