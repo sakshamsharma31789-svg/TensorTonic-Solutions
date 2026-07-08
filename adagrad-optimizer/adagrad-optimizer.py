ba9ee4a13198d43b2fa18d09866114ef12ba527e
sakshamsharma31789-svg <sakshamsharma31789@gmail.com>
2026-07-08 19:30:23 +0530

    Solve adagrad-optimizer

---
+++ adagrad-optimizer/adagrad-optimizer.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def adagrad_step(w, g, G, lr=0.01, eps=1e-8):
+    """
+    Perform one AdaGrad update step.
+    """
+    w = np.array(w)
+    g = np.array(g)
+    G = np.array(G)
+    G = G + g**2
+    w = w -(lr/(np.sqrt(G+eps)))*g
+    return w,G
+    pass
